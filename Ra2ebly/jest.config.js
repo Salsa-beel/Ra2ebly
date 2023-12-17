5cb270155d8beb4a5216f3500c3cae24df9d9ba7
salsabeel <123001325+Salsa-beel@users.noreply.github.com>
2023-12-17 18:53:49 +0200

    added jestjs library

---
+++ Ra2ebly/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['src'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
+  collectCoverage: true,
+  coverageReporters: ['html'],
+};
