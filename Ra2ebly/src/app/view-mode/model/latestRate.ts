d75b3544794abd83cd1440442c03e9fd9078dccf
salsabeel <123001325+Salsa-beel@users.noreply.github.com>
2023-12-17 18:58:03 +0200

    removed

---
+++ Ra2ebly/src/app/view-mode/model/latestRate.ts
@@ -0,0 +1,11 @@
+// export class LatestRate {
+
+//   base: string='';
+//   date:string='';
+//   rates!: {
+//     [currency: string]: number;
+//   };
+//   success: boolean=true;
+//   timestamp: number = 0;
+
+// }
